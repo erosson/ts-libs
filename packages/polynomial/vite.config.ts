2b5c73385ab343e469c9c88ccd1bc6481b0568d5
Evan Rosson <git@erosson.org>
2024-01-05 22:51:04 +0000

    fix: build command

--- packages/polynomial/vite.config.ts
+++ packages/polynomial/vite.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
         // https://vitejs.dev/guide/build.html#library-mode
         lib: {
             entry: resolve(__dirname, 'src/polynomial.ts'),
-            name: "ViteLib",
+            name: "Poly",
+        },
+        rollupOptions: {
+            // allow named exports and a default export without warning me over it.
+            // default export is for common usage, named exports are advanced
+            output: { exports: "named" }
         }
-    }
+    },
 })
