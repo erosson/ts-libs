ba9fdb29a3b75daded431f9ef40e3505dbaba065
Evan Rosson <git@erosson.org>
2024-01-05 23:29:09 +0000

    fix: emit typescript types, source maps, exports

--- packages/polynomial/vite.config.ts
+++ packages/polynomial/vite.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
     build: {
@@ -8,10 +9,12 @@ export default defineConfig({
             entry: resolve(__dirname, 'src/polynomial.ts'),
             name: "Poly",
         },
+        sourcemap: true,
         rollupOptions: {
             // allow named exports and a default export without warning me over it.
             // default export is for common usage, named exports are advanced
             output: { exports: "named" }
         }
     },
+    plugins: [dts({ rollupTypes: true })],
 })
