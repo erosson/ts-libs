db95b0002bebad40a7aca9bfd2dd5ceae65a8c8d
Evan Rosson <git@erosson.org>
2024-01-15 18:48:09 +0000

    poe autopicks latest version

--- packages/arpg-mods/data/pathofexile/poeget.sh
+++ packages/arpg-mods/data/pathofexile/poeget.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-set -eu
+set -eux
 cd "`dirname "$0"`"
 rm -rf dist
 mkdir -p dist
 
+export PATCH="`curl https://raw.githubusercontent.com/poe-tool-dev/latest-patch-version/main/latest.txt`"
+cat config-template.json | envsubst '$PATCH' > config.json
 pathofexile-dat
